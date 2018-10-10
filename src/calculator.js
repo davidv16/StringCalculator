01ec6c60f02907097a35560b3608a7d9da1fe720
david <dabbistandard@Davis-iMac.local>
2018-10-10 22:43:37 +0000

    checks if number includes a separator and returns the sum of the numbers around the separator

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,13 @@
 function add(number){
   if(number == "")  
     return 0;
-
-  return parseInt(number);
+	
+	if(number.includes(","))
+	{
+		var numberArray = number.split(",");
+		return parseInt(numberArray[0]) + parseInt(numberArray[1]));
+	}	
+ return parseInt(number);
 }
 
 
