226d4d66b2e485a0c7fef8f15f0e7d6e396740ee
david <dabbistandard@Davis-iMac.local>
2018-10-10 22:26:58 +0000

    check if calculator returns sum of two given numbers added

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -9,4 +9,6 @@ it("should return number when only one number is in the string", () => {
   expect(add("5")).toBe(5);
 });
 
-
+it("should return sum of two given numbers", () => {
+  expect(add("1,2")).toBe(3);
+});
