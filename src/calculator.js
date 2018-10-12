3eaa7cd88c6922726889fbb999574360124979bd
davidv16 <davidv16@ru.is>
2018-10-12 13:31:59 +0000

    code finalized, cleaned up and commented

--- src/calculator.js
+++ src/calculator.js
@@ -1,37 +1,48 @@
 function add(numbers){
   // is the string empty
-	if(numbers == "")  
+  if(numbers == ""){  
     return 0;
-	let regex = /[\n,]/;
-	if(numbers.substr(0,2) == "//"){
-		regex = numbers.substr(2, numbers.indexOf("\n")-2);
-		numbers = numbers.split('\n')[1];
-	}
-	let	numberArray = numbers.split(regex);
-	let negArray = [];
-	for(let i = 0; i < numberArray.length; i++){
-		let number = parseInt(numberArray[i]);
-		if(number < 0){
-			negArray.push(number);
-		}
-		if(number > 1000){
-			number = 0;
-		}
-		numberArray[i] = number;
-	}
-	if(negArray.length > 0){	
-			throw new Error(`Negatives not allowed: ${negArray.join(',')}`);  			
-	}
-  console.log(numberArray,regex);
-	return sum(numberArray);
+  }
+
+  //is there a delimiter replacement
+  let regex = /[\n,]/;
+  if(numbers.substr(0,2) == "//"){
+    regex = numbers.substr(2, numbers.indexOf("\n")-2);
+    numbers = numbers.split('\n')[1];
+  }
+
+  //splits numbers by delimiter
+  let numberArray = numbers.split(regex);
+  let negArray = [];
+
+  //gather negative numbers if any
+  //is a number bigger than 1000
+  for(let i = 0; i < numberArray.length; i++){
+    let number = parseInt(numberArray[i]);
+    if(number < 0){
+      negArray.push(number);
+    }
+    if(number > 1000){
+      number = 0;
+    }
+    numberArray[i] = number;
+  }
+
+  //is there a negative number
+  if(negArray.length > 0){	
+    throw new Error(`Negatives not allowed: ${negArray.join(',')}`);  			
+  }
+
+  //add numbers together
+  return sum(numberArray);
 }
 
 function sum(numberArray){
-	var total = 0;
+  var total = 0;
   for(var i = 0; i < numberArray.length ; i++){
-		      total += parseInt(numberArray[i]);
-	}
-	return total;
+    total += parseInt(numberArray[i]);
+  }
+  return total;
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -14,29 +14,29 @@ it("should return sum of two given numbers", () => {
 });
 
 it("should sumerize multible numbers", () => {
-	expect(add("2,3,4")).toBe(9);
+  expect(add("2,3,4")).toBe(9);
 });
 
 it("should sumerize multiple numbers with next line as separator", () => {
   expect(add("1\n2,3")).toBe(6);
-	});
-
-it("should throw an exception if number is a negative", () => {
-	const test = () => {
-	add("-1,2")
-	}
-	expect(test).toThrowError('Negatives not allowed: -1');
 });
 
 it("should throw an exception if number is a negative", () => {
   const test = () => {
-	add("2,-4,3,-5")
-	}
-	expect(test).toThrowError('Negatives not allowed: -4,-5');
+  add("-1,2")
+  }
+    expect(test).toThrowError('Negatives not allowed: -1');
+});
+
+it("should throw an exception if number is a negative", () => {
+  const test = () => {
+    add("2,-4,3,-5")
+  }
+    expect(test).toThrowError('Negatives not allowed: -4,-5');
 });
 
 it("should return 2 if number goes above a 1000", () => {
-	expect(add("1001,2")).toBe(2);
+  expect(add("1001,2")).toBe(2);
 });
 
 it("should return 3 if delimiter is a semi colon", () => {
